687dbf433057fea30ecace1dbc183b054c6de087
Ben Clarke <ben@upliftingmedia.co.uk>
2016-11-07 18:02:16 +0000

    css test fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
         <title>Investorex</title>
 
-        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('../css/app.css') }}">
     </head>
     <body>
 
