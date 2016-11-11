69b88fef8d40bd7d036d6670fafb5acf1d728578
Ben Clarke <ben@upliftingmedia.co.uk>
2016-11-11 16:57:04 +0000

    readded name instance

--- config/mail.php
+++ config/mail.php
@@ -19,7 +19,7 @@ return array(
   'driver' => 'smtp',
   'host' => 'smtp.sendgrid.net',
   'port' => 587,
-  'from' => array('address' => 'ben@upliftingmedia.co.uk'),
+  'from' => array('address' => 'ben@upliftingmedia.co.uk', 'name' => 'Ben Clarke'),
   'encryption' => 'tls',
   'username' => env('SENDGRID_USERNAME'),
   'password' => env('SENDGRID_PASSWORD'),
