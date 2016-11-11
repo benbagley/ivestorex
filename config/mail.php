f63420c3ec9728804b3d980bbff2de6c8e0bea77
Ben Clarke <ben@upliftingmedia.co.uk>
2016-11-11 16:37:27 +0000

    swapped out send grid

--- config/mail.php
+++ config/mail.php
@@ -1,28 +1,26 @@
 <?php
 
+// TESTING //
+// return array(
+//   "driver" => "smtp",
+//   "host" => "mailtrap.io",
+//   "port" => 2525,
+//   "from" => array(
+//       "address" => "ben@upliftingmedia.co.uk",
+//       "name" => "Ben Clarke"
+//   ),
+//   "username" => "28cd7ed6c35040",
+//   "password" => "c8c23949733432",
+//   "sendmail" => "/usr/sbin/sendmail -bs",
+//   "pretend" => false
+// );
+
 return array(
-  "driver" => "smtp",
-  "host" => "mailtrap.io",
-  "port" => 2525,
-  "from" => array(
-      "address" => "ben@upliftingmedia.co.uk",
-      "name" => "Ben Clarke"
-  ),
-  "username" => "28cd7ed6c35040",
-  "password" => "c8c23949733432",
-  "sendmail" => "/usr/sbin/sendmail -bs",
-  "pretend" => false
+  'driver' => 'smtp',
+  'host' => 'smtp.sendgrid.net',
+  'port' => 587,
+  'from' => array('address' => 'contact_address'),
+  'encryption' => 'tls',
+  'username' => 'SENDGRID_USERNAME',
+  'password' => 'SENDGRID_PASSWORD',
 );
-
-
-if (App::environment('production')) {
-    return array(
-      'driver' => 'smtp',
-      'host' => 'smtp.sendgrid.net',
-      'port' => 587,
-      'from' => array('address' => 'contact_address'),
-      'encryption' => 'tls',
-      'username' => 'SENDGRID_USERNAME',
-      'password' => 'SENDGRID_PASSWORD',
-    );
-}
