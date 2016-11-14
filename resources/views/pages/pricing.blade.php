59f4c75073367836e36e101a56339b9cc798d917
Ben Clarke <ben@upliftingmedia.co.uk>
2016-11-14 13:25:00 +0000

    pricing page complete, just need to add payment gateway

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -9,4 +9,187 @@
             </div>
         </div>
     </div>
+
+    <div id="pricing-plans">
+        <div class="grid">
+            <div class="grid__col grid__col--1-of-3">
+                <div class="silver">
+                    <h1 class="title">Silver</h1>
+                    <h2 class="bold">$500</h2>
+
+                    <table>
+                        <tr>
+                            <td>
+                                <p>Access to our full training course</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>one-on-one e-mail support</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to trading groups</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>A shiny forum badge</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to the private lounge on our forums</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Regular Hangouts<p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Granted access to some of the most successful traders in our group</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#" class="button-primary">Start Now</a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="grid__col grid__col--1-of-3">
+                <div class="gold">
+                    <h1 class="title">Gold</h1>
+                    <h2 class="bold">$750</h2>
+
+                    <table>
+                        <tr>
+                            <td>
+                                <p>Access to our full training course</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>one-on-one e-mail support</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to trading groups</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>A shiny forum badge</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to the private lounge on our forums</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Regular Hangouts<p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Granted access to some of the most successful traders in our group</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>A weekly live analysis sessions with our top fund managers</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Get trade alerts each time a member of staff enters a trade, so you can earn while you learn</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#" class="button-primary">Start Now</a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="grid__col grid__col--1-of-3">
+                <div class="platinum">
+                    <h1 class="title">Platinum</h1>
+                    <h2 class="bold">$1000</h2>
+
+                    <table>
+                        <tr>
+                            <td>
+                                <p>Access to our full training course</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>one-on-one e-mail support</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to trading groups</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>A shiny forum badge</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Access to the private lounge on our forums</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Regular Hangouts<p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Granted access to some of the most successful traders in our group</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>A weekly live analysis sessions with our top fund managers</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Get trade alerts each time a member of staff enters a trade, so you can earn while you learn</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p>Get access to a Fund Manager and out bootcamp course
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#" class="button-primary">Start Now</a>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="disclaimer">
+        <p class="bold">If you are learning with the goal of becoming a Fund Manager in mind, this course is for you. Our current Fund Mangers will work hand in hand and will personally help build you up to the point where you will receive a fund from Investorex to trade, manage, and grow. (This is risk free and very profitable for you)</p>
+    </div>
 @endsection
